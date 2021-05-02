4eeddb987f32239a01da0eefe35c0e020b36f4cd
Athul Cyriac Ajay <athul8720@gmail.com>
2021-05-02 11:29:29 +0530

    Removed Error prone code

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
+from typing import Any, Dict, Optional
 import requests
 import pytz
+import os
 from datetime import date,datetime
-from deta import App
+from deta import App #comment this when you're developing locally
 
-app = App()
+app = App() #comment this when you're developing locally
+
+token:str = os.getenv('TOKEN')
+chat_id:str = os.getenv('CHAT_ID')
 
 def sendTGMessage(message:str)->None:
-    url = f'https://api.telegram.org/bot1287952207:AAG3BjXt1XTBorfvRPwE-zFFd-XBCtIwXdY/sendMessage'
-    msg_data = {'chat_id':"523986211",'text':message,"parse_mode":"Markdown"}
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    msg_data = {'chat_id':chat_id,'text':message,"parse_mode":"Markdown"}
     resp = requests.post(url, msg_data).json()
     print("Message Not Send" if resp['ok'] is False else "👉    Message Sent")
 
-@app.lib.cron()
-def getCowinData(event):
+@app.lib.cron() #comment this when you're developing locally
+def getCowinData(event:Optional[str]=None)->None:
     tz = pytz.timezone("Asia/Calcutta")
     time= datetime.now(tz).strftime('%H:%M:%S')
-    date = date.today().strftime("%d-%m-%Y")
-    data = requests.get(f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=304&date={date}").json()
+    tdate = date.today().strftime("%d-%m-%Y")
+    data = requests.get(f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=304&date={tdate}").json()
     nos_centers = len(data['centers'])
     print(nos_centers)
-    message = f"No Vaccination Centers Available in Kottayam \n\nat {time} on {date}" if nos_centers<=0 else f"{nos_centers} Available in Kottayam on {date} at {time}"
-    sendTGMessage(message)
+    message = f"No Vaccination Centers Available in Kottayam \n\nat {time} on {tdate}" if nos_centers<=0 else f"{nos_centers} Available in Kottayam on {tdate} at {time}"
+    sendTGMessage(message)    
+
+if __name__ == "__main__":
+    """ Uncomment these lines when deploying
+    """
+    getCowinData()
